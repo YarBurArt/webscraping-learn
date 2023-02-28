5dca755db64d09462c4f62b4579e4a918348d45e
Yaroslav <81087967+YarBurArt@users.noreply.github.com>
2023-02-28 08:36:37 +0500

    Update gparse.py

--- parsing/gparse.py
+++ parsing/gparse.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 query = input('Search: ')
 query = query.replace(' ', '+')
 url = URL = f"https://google.com/search?q={query}"
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+USER_AGENT = "Mozilla/5.0 (Macintosh;"
+            " Intel Mac OS X 10.14; rv:65.0)"
+            " Gecko/20100101 Firefox/65.0"
 headers = {"user-agent": USER_AGENT}
 snew_class = 'kvH3mc'  # edit if don't work
 
