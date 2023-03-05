74529a120696264eaae7f705ec4cc39b5559e686
yarburart <yaroslav.chayk@gmail.com>
2023-03-05 11:29:41 +0500

    debug

---
+++ utils/send_mail.py
@@ -0,0 +1,29 @@
+from email.mime.text import MIMEText
+from smtplib import SMTP
+from os import getenv
+
+
+def send_email(message):
+    sender = "yaroslav.chayk@gmail.com"
+    password = getenv("EMAIL_PASSWORD")
+    server = SMTP("smtp.gmail.com", 587)
+
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        msg = MIMEText(message)
+        msg["Subject"] = "CLICK ME PLEASE!"
+        server.sendmail(sender, sender, msg.as_string())
+        # server.sendmail(sender, sender, f"Subject: CLICK ME PLEASE!\n{message}")
+        return "The message was sent successfully!"
+    except Exception as _ex:
+        return f"{_ex}\nCheck your login or password please!"
+
+
+if __name__ == "__main__":
+    mess = input("Type your message: ")
+    print(send_email(message=mess))
+
+import os
+os.environ["DEBUSSY"] = "1"
