31e49bbcd95563337fe73ca70d6d7a2b00640a67
YarBurArt <yaroslav.chayk@gmail.com>
2023-03-06 20:29:44 +0500

    add avito parsing n clear code

--- parsing/booksave.py
+++ parsing/booksave.py
@@ -46,10 +46,10 @@ def stolen_library():
                 "title": trans_title,
                 "page": page,
                 "link": url,
-                "body": outtext,
+                # "body": outtext,
             })
 
-        with open(f"data/test_pg_book.json", 'a', encoding="utf-8") as file:
+        with open(f"data/lib_pg_book.json", 'a', encoding="utf-8") as file:
             json.dump(data, file,
                       ensure_ascii=False)
 
