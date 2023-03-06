616efbe49f3bb5f0b7b5eaa91770b243f6f55575
YarBurArt <yaroslav.chayk@gmail.com>
2023-03-06 16:38:23 +0500

    add work for

--- parsing/booksave.py
+++ parsing/booksave.py
@@ -3,44 +3,59 @@ from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
 import json
 
+from progress.bar import Bar
 
-def test_page():
-    work = 1
+bar = Bar("Check and downloading", max=1000000)
+
+
+def stolen_library():
     data = []
-    for page in range(1, 1000):
-        # page = 1
-        url = f"https://ilibrary.ru/text/{str(work)}/p.{str(page)}/index.html"
-        try:
-            page_html = rq.get(url)
-        except rq.exceptions.InvalidURL:
-            break
-        if page_html.status_code != 200:
-            break
+    for work in range(1, 1000):
+        for page in range(1, 1000):
+            # page = 1 / work = 1
+            url = f"https://ilibrary.ru/text/{str(work)}/p.{str(page)}/index.html"
+            try:
+                page_html = rq.get(url)
+                bar.next()
+            except rq.exceptions.InvalidURL:
+                bar.next()
+                break
+            if page_html.status_code != 200:
+                bar.next()
+                break
 
-        soup = BeautifulSoup(page_html.text, 'lxml')
-        outtext = ''
+            soup = BeautifulSoup(page_html.text, 'lxml')
+            outtext = ''
 
-        for pargf in soup.find_all('span', class_='p'):
-            outtext += pargf.text
+            for pargf in soup.find_all('span', class_='p'):
+                outtext += pargf.text
 
-        # translate_text = GoogleTranslator(source='auto', target='en').translate(outtext)
-        print(outtext)
+            translator = GoogleTranslator(source='auto', target='en')
 
-        strip_title = soup.title.text.replace('. Текст произведения', '').replace(' ', '').lower()
-        trans_title = GoogleTranslator(source='auto', target='en').translate(strip_title)
-        print(trans_title)
+            # translate_text = translator.translate(outtext)
+            # print(outtext)
+            # ot_text = ""
+            # for i in outtext.split("."):
+            #     ot_text = ot_text + translator.translate(i) + "."
 
-        data.append({
-            "title": trans_title,
-            "page": page,
-            "link": url,
-            "body": outtext,
-        })
+            strip_title = soup.title.text.replace('. Текст произведения', '').replace(' ', '').lower()
+            trans_title = translator.translate(strip_title)
+            print(trans_title)
 
-    with open(f"data/test_pg_book.json", 'w') as file:
-        json.dump(data, file)
+            data.append({
+                "title": trans_title,
+                "page": page,
+                "link": url,
+                "body": outtext,
+            })
+
+        with open(f"data/test_pg_book.json", 'a', encoding="utf-8") as file:
+            json.dump(data, file,
+                      ensure_ascii=False)
+
+    bar.finish()
 
 
 if __name__ == '__main__':
-    test_page()
+    stolen_library()
 
