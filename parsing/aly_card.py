83da128f5dc20a1700ff6ad3ea83616943236bd4
YarBurArt <yaroslav.chayk@gmail.com>
2023-03-19 18:03:30 +0500

    add ali product parser

---
+++ parsing/aly_card.py
@@ -0,0 +1,56 @@
+import requests as rq
+from bs4 import BeautifulSoup
+
+import pandas as pd
+
+# edit if aliexpress modification style css selectors
+TITLE_CLASS = "snow-ali-kit_Typography__base__1shggo"
+PRICE_CLASS = "snow-price_SnowPrice__mainS__azqpin"
+STATE_CLASS = "SnowSku_SkuPropertyItem__propNameWrap__sj6tl"
+
+data = {
+    'Name': [],
+    'Price': [],
+    'From state': []
+}
+
+
+def get_page() -> BeautifulSoup:
+    """The function for save html page to RAM"""
+    #https://aliexpress.ru/item/2044891395.html?sub=44981&utm_campaign=44981&af=739_44981&aff_platform=api-new-link-generate&utm_medium=cpa&sub1=1068989&cn=2ururq6bi16k03uyd6y2hllw354pbvgy&dp=2ururq6bi16k03uyd6y2hllw354pbvgy&aff_fcid=0ca158e4d6084f35acb0f1ada599675d-1676551609365-08780-_DEkTcIR&cv=3&aff_fsk=_DEkTcIR&sk=_DEkTcIR&aff_trace_key=0ca158e4d6084f35acb0f1ada599675d-1676551609365-08780-_DEkTcIR&terminal_id=ccde0aed126d4739a41595ddbbb4047c&utm_content=1068989&utm_source=admitad&gatewayAdapt=glo2rus&sku_id=12000024771914456
+    url = input("url: ")
+    page = rq.get(url)
+    return BeautifulSoup(page.text, 'lxml')
+
+
+def find_elements(soup: BeautifulSoup):
+    """The function for find elements and add to data"""
+    global data
+
+    try:
+        title = soup.find('h1', class_=TITLE_CLASS)
+        # print(title.text)
+        price = soup.find('div', class_=PRICE_CLASS)
+        # print(price.text)
+        from_state = soup.find('div', class_=STATE_CLASS)
+        # print(from_state.text[11:])
+        # ...
+        data['Name'].append(title)
+        data['Price'].append(price)
+        data['From state'].append(from_state)
+
+    except AttributeError:
+        print("site block me, try add ebalay magic")
+
+
+def export_exel(data_s: pd.DataFrame):
+    data_s.to_excel("data/ali_data_product.xlsx")
+
+
+if __name__ == "__main__":
+    my_soup = get_page()
+    find_elements(my_soup)
+
+    if input("save (y, n)? ") == 'y':
+        export_exel(data)
+
---
+++ parsing/health_parse.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.ecpi.edu/blog/how-can-a-software-developer-work-in-healthcare"
+
+
