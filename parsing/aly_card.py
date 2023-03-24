cca84749fe88a088d58c280dadbbd85ea7da92fa
YarBurArt <yaroslav.chayk@gmail.com>
2023-03-24 14:18:15 +0500

    types

--- parsing/aly_card.py
+++ parsing/aly_card.py
@@ -1,25 +1,26 @@
 import requests as rq
+from requests.models import Response
 from bs4 import BeautifulSoup
 
 import pandas as pd
 
 # edit if aliexpress modification style css selectors
-TITLE_CLASS = "snow-ali-kit_Typography__base__1shggo"
-PRICE_CLASS = "snow-price_SnowPrice__mainS__azqpin"
-STATE_CLASS = "SnowSku_SkuPropertyItem__propNameWrap__sj6tl"
+TITLE_CLASS: str = "snow-ali-kit_Typography__base__1shggo"
+PRICE_CLASS: str = "snow-price_SnowPrice__mainS__azqpin"
+STATE_CLASS: str = "SnowSku_SkuPropertyItem__propNameWrap__sj6tl"
 
-data = {
-    'Name': [],
-    'Price': [],
-    'From state': []
+data: dict = {
+    'Name': list[str],
+    'Price': list[str],
+    'From state': list[str]
 }
 
 
 def get_page() -> BeautifulSoup:
     """The function for save html page to RAM"""
     #https://aliexpress.ru/item/2044891395.html?sub=44981&utm_campaign=44981&af=739_44981&aff_platform=api-new-link-generate&utm_medium=cpa&sub1=1068989&cn=2ururq6bi16k03uyd6y2hllw354pbvgy&dp=2ururq6bi16k03uyd6y2hllw354pbvgy&aff_fcid=0ca158e4d6084f35acb0f1ada599675d-1676551609365-08780-_DEkTcIR&cv=3&aff_fsk=_DEkTcIR&sk=_DEkTcIR&aff_trace_key=0ca158e4d6084f35acb0f1ada599675d-1676551609365-08780-_DEkTcIR&terminal_id=ccde0aed126d4739a41595ddbbb4047c&utm_content=1068989&utm_source=admitad&gatewayAdapt=glo2rus&sku_id=12000024771914456
-    url = input("url: ")
-    page = rq.get(url)
+    url: str = input("url: ")
+    page: Response = rq.get(url)
     return BeautifulSoup(page.text, 'lxml')
 
 
@@ -28,11 +29,11 @@ def find_elements(soup: BeautifulSoup):
     global data
 
     try:
-        title = soup.find('h1', class_=TITLE_CLASS)
+        title: str = soup.find('h1', class_=TITLE_CLASS).text
         # print(title.text)
-        price = soup.find('div', class_=PRICE_CLASS)
+        price: str = soup.find('div', class_=PRICE_CLASS).text
         # print(price.text)
-        from_state = soup.find('div', class_=STATE_CLASS)
+        from_state: str = soup.find('div', class_=STATE_CLASS).text
         # print(from_state.text[11:])
         # ...
         data['Name'].append(title)
