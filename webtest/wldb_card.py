a91b7966447eca7653913dbaea32bb64a6d7b695
YarBurArt <yaroslav.chayk@gmail.com>
2023-04-13 18:01:34 +0500

    excel wldt

--- webtest/wldb_card.py
+++ webtest/wldb_card.py
@@ -8,6 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 import json
+import pandas as pd
 
 
 class MyTestCase(unittest.TestCase):
@@ -24,7 +25,7 @@ class MyTestCase(unittest.TestCase):
         detail_desc_elem = driver.find_element(By.CLASS_NAME, "j-description-btn")
         detail_param_elem.click()
         detail_desc_elem.click()
-        time.sleep(10)
+        time.sleep(5)
 
         soup = BeautifulSoup(driver.page_source, 'lxml')
         # print(soup)
@@ -32,26 +33,47 @@ class MyTestCase(unittest.TestCase):
         print(description)
 
         data = []
+        result_params = {}
         tables = soup.find_all('table', class_="product-params__table")
+
         for table in tables:
             table_body = table.find('tbody')
-
             rows = table_body.find_all('tr')
+
             for row in rows:
+                cols = row.find_all('th')
+                cols = [ele.text.strip() for ele in cols]
+                data.append([ele for ele in cols if ele])  # Get rid of empty values
+                del cols
                 cols = row.find_all('td')
                 cols = [ele.text.strip() for ele in cols]
                 data.append([ele for ele in cols if ele])  # Get rid of empty values
+
+        for i in range(0, len(data) - 1, 2):
+            result_params[str(data[i][0])] = data[i + 1][0]
 
         print(data)
 
         out_data = {
             "description": description,
-            "params": data,
+            "params": result_params,
         }
-        out_data = json.dumps(out_data, sort_keys=False,
-                              indent=4, ensure_ascii=False).encode("utf8")
-        with open("data/wldt.json", "w") as write_file:
-            json.dump(out_data, write_file)
+
+        with open("data/wldt.json", "w", encoding='utf-8') as write_file:
+            json.dump(out_data, write_file, sort_keys=False,
+                      indent=4, ensure_ascii=False)
+
+        result_params["description"] = description
+        out_data = {
+            "name": [],
+            "param": []
+        }
+        for x, y in result_params.items():
+            out_data["name"].append(x)
+            out_data["param"].append(y)
+
+        df = pd.DataFrame(out_data)
+        df.to_excel('data/wldt.xlsx')
 
     def tearDown(self):
         self.driver.close()
