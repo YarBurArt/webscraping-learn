93e3f49190f86e855f7de7d2587802ebbee4f541
Yaroslav <81087967+YarBurArt@users.noreply.github.com>
2023-05-05 12:42:36 +0500

    Update selka.py

--- webtest/selka.py
+++ webtest/selka.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+# test: selenium is work? and mini cheatsheet
+
 
 profile_path = r'C:\\Users\\user\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\1793b34j.default'
 options = Options()
