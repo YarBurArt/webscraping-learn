6f305f11468b39bf6de4d52a83d0b9d7ac8441b7
YarBurArt <yaroslav.chayk@gmail.com>
2023-06-28 20:14:17 +0500

    loging booksave

--- parsing/booksave.py
+++ parsing/booksave.py
@@ -1,10 +1,16 @@
+import json
+import logging
+
 import requests as rq
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
-import json
 
 from progress.bar import Bar
 
+
+logging.basicConfig(level=logging.INFO, filename="logs/booksave_log.log", filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
+logging.info("start app")
 bar = Bar("Check and downloading", max=10000)
 
 
@@ -17,18 +23,23 @@ def stolen_library():
             try:
                 page_html = rq.get(url)
                 bar.next()
+                logging.info(f"OK request url: {url}")
             except rq.exceptions.InvalidURL:
                 bar.next()
+                logging.info(f"Invalid url: {url}")
                 break
             if page_html.status_code != 200:
                 bar.next()
+                logging.info(f"Unsuccessful request by url: {url}")
                 break
 
             soup = BeautifulSoup(page_html.text, 'lxml')
             outtext = ''
-
-            for pargf in soup.find_all('span', class_='p'):
-                outtext += pargf.text
+            try:
+                for pargf in soup.find_all('span', class_='p'):
+                    outtext += pargf.text
+            except (AttributeError, TypeError) as e:
+                logging.error("CSS selector ERROR")
 
             translator = GoogleTranslator(source='auto', target='en')
 
@@ -41,6 +52,7 @@ def stolen_library():
             strip_title = soup.title.text.replace('. Текст произведения', '').replace(' ', '').lower()
             trans_title = translator.translate(strip_title)
             print(trans_title)
+            logging.debug("text translated")
 
             data.append({
                 "title": trans_title,
@@ -48,14 +60,20 @@ def stolen_library():
                 "link": url,
                 # "body": outtext,
             })
-
-        with open(f"data/lib_pg_book.json", 'a', encoding="utf-8") as file:
-            json.dump(data, file,
-                      ensure_ascii=False)
+        try:
+            # with open(f"data/lib_pg_book.json", 'a', encoding="utf-8") as file:
+            #     json.dump(data, file,
+            #               ensure_ascii=False)
+            logging.info("File saved successfully")
+        except (FileExistsError, OSError, InterruptedError) as e:
+            logging.error("File was not saved, ERROR: {e}")
 
     bar.finish()
 
 
 if __name__ == '__main__':
-    stolen_library()
-
+    try:
+        stolen_library()
+        logging.info("down app")
+    except KeyboardInterrupt as e:
+        logging.warning("Stopped from user")
